e742ff2fc55c769d5fb850985fa133e97fe9ea9c
Sandy <sschillebeeckx@abis.be>
2020-10-07 18:31:25 +0200

    B1

---
+++ ExB1/src/main/java/be/abis/exercise/ExB1Application.java
@@ -0,0 +1,14 @@
+package be.abis.exercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExB1Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ExB1Application.class, args);
+	}
+
+	
+}
